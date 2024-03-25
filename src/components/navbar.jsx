d4f47955410aad563241c5cfc795c3df81a0199c
Shawn Kebel <fxrender@outlook.com>
2024-03-25 10:43:38 -0500

    :tada: Begin Nextjs `personal portfolio`

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* LOGO */}
+      <div className="">1</div>
+
+      {/* RESPONSIVE MENU */}
+      <div className="">2</div>
+    </div>
+  );
+};
+
+export default Navbar;
