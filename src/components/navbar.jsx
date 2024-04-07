eab7344115764d171534009e7757894624eb3b0f
Shawn Kebel <fxrender@outlook.com>
2024-04-06 23:51:37 -0500

    :recycle: Remove unnecessary code and `adjust alignment` in navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -71,14 +71,8 @@ const Navbar = () => {
 
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
-      {/* LINKS */}
-      <div className="hidden md:flex gap-4 w-1/3">
-        {links.map((link) => (
-          <NavLink link={link} key={link.title} />
-        ))}
-      </div>
       {/* LOGO */}
-      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+      <div className="md:flex xl:w-1/3 xl:justify-left">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -89,8 +83,16 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
-      {/* SOCIAL */}
+
+      {/* LINKS */}
       <div className="hidden md:flex gap-4 w-1/3">
+        {links.map((link) => (
+          <NavLink link={link} key={link.title} />
+        ))}
+      </div>
+
+      {/* SOCIAL */}
+      <div className="hidden md:flex md:justify- gap-4 w-1/3">
         <Link href="https://github.com/Jackal73">
           <Image src="/github.png" alt="" width={24} height={24} />
         </Link>
@@ -103,9 +105,9 @@ const Navbar = () => {
         <Link href="https://facebook.com/ShawnKebel">
           <Image src="/facebook.png" alt="" width={24} height={24} />
         </Link>
-        <Link href="https://dribble.com">
+        {/* <Link href="https://dribble.com">
           <Image src="/dribbble.png" alt="" width={24} height={24} />
-        </Link>
+        </Link> */}
         <Link href="https://pinterest.com">
           <Image src="/pinterest.png" alt="" width={24} height={24} />
         </Link>
